743fa0982c38650ba7a5954c87aff232ff743b52
Avery Chan <JustinAveryChan@gmail.com>
2021-08-15 21:42:01 -0500

    feat: add skeleton app textarea
    build: add styled components

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,42 +1,27 @@
 import React from 'react';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import styled from 'styled-components';
+import InputArea from './InputArea';
 import './App.global.css';
+
+const LayoutContainer = styled.div`
+  overflow-y: hidden;
+  display: grid;
+  grid-template-rows: 2;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  margin: auto;
+  padding: 15px;
+`;
 
 const Hello = () => {
   return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
+    <LayoutContainer>
+      <Title>ThoughtApp</Title>
+      <InputArea />
+    </LayoutContainer>
   );
 };
 
---
+++ src/renderer/InputArea.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Area = styled.textarea`
+  width: 90%;
+  height: 80vh;
+  background-color: white;
+  margin: auto;
+`;
+
+const InputArea = () => {
+  return (
+    <>
+      <Area />
+    </>
+  );
+};
+
+export default InputArea;
